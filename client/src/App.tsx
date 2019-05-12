e84bd40018d181c1994cff1bf986cea2c82de5cd
Matt Wright <mattwright3195@gmail.com>
2019-05-11 23:31:02 -0700

    typescript and relay setup

---
+++ client/src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return <div className="">Hello World</div>;
+};
+
+export default App;
+
+// console.log(process.env.REACT_APP_NOT_SECRET_CODE);
+// "proxy": "http://localhost:0000" in package.json
+// import graphql from 'babel-plugin-relay/macro';
+
+// const print = <T extends {}>(msg: T): T => {
+//   return msg;
+// };
+
+// const print = function<T>(msg: T): T {
+//   return msg;
+// };
+
+// console.log(print(84545));
